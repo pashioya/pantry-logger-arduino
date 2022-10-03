9788b149f15ac8f1b71c453e267a1590f62e36b5
tomellm <thomas.ellmenreich@gmail.com>
2022-10-03 13:12:59 +0200

    test setup screen, potentiometer, buttons

--- include/Util.h
+++ include/Util.h
@@ -1,3 +1,7 @@
 #include <Arduino.h>
 
 String numToString(int number, int size, char padChar);
+
+String getCharLine(char repeatChar);
+
+void changeLine(LiquidCrystal screenObj, int line, char repeatChar);
